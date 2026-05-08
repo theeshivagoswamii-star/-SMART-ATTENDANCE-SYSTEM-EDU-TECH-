649b8090c6d894b0078bd2fa332fb42f6e53b09b
Shiva Goswami <theeshivagoswamii@gmail.com>
2026-05-08 21:55:10 +0530

    Add files via upload

---
+++ logout.php
@@ -0,0 +1,9 @@
+<?php
+// ============================================
+// Logout
+// ============================================
+session_start();
+session_destroy();
+header('Location: ../frontend/login.html');
+exit;
+?>
