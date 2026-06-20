836a21ec4f714a484afa36e5970c3c864f54c8c8
Shiva Goswami <theeshivagoswamii@gmail.com>
2026-06-20 16:01:38 +0530

    Delete logout.php

--- logout.php
+++
@@ -1,9 +0,0 @@
-<?php
-// ============================================
-// Logout
-// ============================================
-session_start();
-session_destroy();
-header('Location: ../frontend/login.html');
-exit;
-?>
